722f2351b7ff05315a77e44fcb151d0ca6e4a40e
Polina Che <perhaybe@gmail.com>
2023-06-04 20:40:30 +0300

    feat: persist-settings

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 
 import { Provider } from 'react-redux';
-import { store } from './store';
+import { store, persistor } from './store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+      <PersistGate persistor={persistor} loading={null}>
         <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
--- src/store.js
+++ src/store.js
@@ -1,9 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
 import { filterReducer } from 'features/filter/filter-slice';
 import { positionsReducer } from 'features/positions/position-slice';
 
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const rootReducer = combineReducers({
+  filters: filterReducer,
+  positions: positionsReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
   devTools: true,
-  reducer: { filters: filterReducer, positions: positionsReducer },
+  reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
